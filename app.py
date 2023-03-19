eaf1f448409a719daa8f133f33d556e2b7247128
FZNH97 <70118653+bradscot@users.noreply.github.com>
2023-03-19 14:55:22 -0500

    crossed

--- app.py
+++ app.py
@@ -25,12 +25,22 @@ def hello():
         database = 'quotechies-db'
         username = 'bscott129@quotechies'
         password = 'hackathon10!'
-        driver = '{ODBC Driver 17 for SQL Server}'
-        cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
-        cursor = cnxn.cursor()
-        cursor.execute("INSERT INTO session (zip_code) VALUES (?)", zip)
-        cnxn.commit()
+        driver= '{ODBC Driver 17 for SQL Server}'
 
+        # Create the connection string
+        connection_string = 'DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password
+
+        # Set up the connection
+        conn = pyodbc.connect(connection_string)
+        cursor = conn.cursor()
+
+        #cursor.execute("IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='session' and xtype='U') CREATE TABLE session (zip_code varchar(10), session_id varchar(255))")
+        cursor.execute("INSERT INTO session (zip_code) VALUES (?)", zip)
+        conn.commit()
+        # Commit the transaction
+        cursor.close()
+        # Close the database connection
+        conn.close()
 
         return render_template('hello.html', zip=zip)
     else:
