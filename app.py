26ebeed0accd9a6f4845c1d3f68bb57669f706a6
FZNH97 <70118653+bradscot@users.noreply.github.com>
2023-03-19 21:48:24 -0500

    push

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import os
 import pandas as pd
@@ -6,6 +5,8 @@ import matplotlib.pyplot as plt
 from sqlalchemy import create_engine, text
 import io
 import base64
+import traceback
+import sys
 
 
 
@@ -17,30 +18,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    print('Request for index page received')
     return render_template('index.html')
-
-
-
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 
 
 @app.route('/hello', methods=['POST'])
 def hello():
-    zip = request.form.get('zip')
-    if zip:
-        print('Request for hello page received with zip=%s' % zip)
+    try:
+        zip_code = request.form.get('zip')
+        if not zip_code:
+            return redirect(url_for('index'))
 
 
-
-
-        # Insert the zip code into the database
         server = 'quotechies.database.windows.net'
         database = 'quotechies-db'
         username = 'bscott129@quotechies'
@@ -48,29 +38,15 @@ def hello():
         driver = '{ODBC Driver 17 for SQL Server}'
 
 
-
-
-        # Create the connection string
         connection_string = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}"
-
-
-
-
-        # Set up the connection
         engine = create_engine(connection_string)
         conn = engine.connect()
 
 
-
-
-        # Insert zip code
         query = text("INSERT INTO session2 (zip_code) VALUES (:zip)")
-        conn.execute(query, zip=zip)
+        conn.execute(query, zip=zip_code)
 
 
-
-
-        # Query the data for the graph
         query = text("""
             SELECT Season, Reason, AVG(Avg_Price)
             FROM weather_incidents
@@ -86,9 +62,6 @@ def hello():
         data = conn.execute(query)
 
 
-
-
-        # Create a dictionary to store the data
         graph_data = {}
         for row in data:
             season, reason, avg_price = row
@@ -102,52 +75,52 @@ def hello():
             graph_data[reason][season.lower()] = avg_price
 
 
-
-
-        # Close the database connection
         conn.close()
 
 
-
-
-        # Create a line chart
-        fig, ax = plt.subplots()
+        x_labels = ['Spring', 'Summer', 'Fall', 'Winter']
+        datasets = []
         for reason, values in graph_data.items():
-            x = ['Spring', 'Summer', 'Fall', 'Winter']
-            y = [values['Spring'], values['Summer'], values['Fall'], values['Winter']]
-            ax.plot(x, y, label=reason)
-        ax.set_xlabel('Season')
-        ax.set_ylabel('Average Price')
-        ax.set_title('Energy Surge Costs Due to Outages by Season')
-        ax.legend()
+            data = [values['spring'], values['summer'], values['fall'], values['winter']]
+            datasets.append({
+                'label': reason,
+                'data': data,
+                'borderColor': 'red',
+                'fill': False
+            })
+
+
+        chart_data = {
+            'type': 'line',
+            'data': {
+                'labels': x_labels,
+                'datasets': datasets
+            },
+            'options': {
+                'scales': {
+                    'yAxes': [{
+                        'ticks': {
+                            'beginAtZero': True
+                        }
+                    }]
+                }
+            }
+        }
+
+
+        return render_template('hello.html', zip=zip_code, chart_data=chart_data)
+
+
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
+        return render_template('error.html')
 
 
 
 
-        # Encode the image as a base64 string
-        buffer = io.BytesIO()
-        fig.savefig(buffer, format='png')
-        buffer.seek(0)
-        image_string = base64.b64encode(buffer.getvalue()).decode()
-
-
-
-
-        # Render the template with the image embedded
-        return render_template('hello.html', zip=zip, image_string=image_string)
-
-
-
-
-    else:
-        print('Request for hello page received with no name or blank name -- redirecting')
-        return redirect(url_for('index'))
-
-
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 
