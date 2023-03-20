175aa4752360978639f5a568c03f4a52579268c4
FZNH97 <70118653+bradscot@users.noreply.github.com>
2023-03-20 01:55:12 -0500

    cleaned it up

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory,jsonify
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from sqlalchemy import create_engine, text
-import io
-import base64
-import traceback
-import sys
-import json
 import csv
-import numpy
+import json
+
 
 app = Flask(__name__)
 app.config['REFERRER_POLICY'] = 'no-referrer-when-downgrade'
+
 
 def read_csv_file(filename):
     with open(filename, 'r') as f:
         reader = csv.DictReader(f)
         data = [row for row in reader]
     return data
+
 
 @app.route('/hello', methods=['POST'])
 def hello():
@@ -27,26 +25,26 @@ def hello():
         return redirect(url_for('index'))
 
     # Read data from CSV file
-    data = read_csv_file('static\data\outage_mock.csv')
+    data = read_csv_file(os.path.join(app.static_folder, 'data', 'outage_mock.csv'))
 
     graph_data = {}
     for row in data:
-        Season, Reason, Total_Cost = row['Season'], row['Reason'], float(row['Total_Cost'])
-        if Reason not in graph_data:
-            graph_data[Reason] = {
+        season, reason, total_cost = row['Season'], row['Reason'], float(row['Total_Cost'])
+        if reason not in graph_data:
+            graph_data[reason] = {
                 'Spring': 0,
                 'Summer': 0,
                 'Fall': 0,
                 'Winter': 0
             }
-        graph_data[Reason][Season] += Total_Cost
+        graph_data[reason][season] += total_cost
 
     x_labels = ['Spring', 'Summer', 'Fall', 'Winter']
     datasets = []
-    for Reason, values in graph_data.items():
+    for reason, values in graph_data.items():
         data = [values['Spring'], values['Summer'], values['Fall'], values['Winter']]
         datasets.append({
-            'label': Reason,
+            'label': reason,
             'data': data,
             'borderColor': 'red',
             'fill': False
@@ -73,9 +71,19 @@ def hello():
 
     return render_template('hello.html', zip=zip_code, chart_data=chart_json)
 
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
 
 
 
