eee5dcce0df36b099f374142e6bcb09cb9501dcf
FZNH97 <70118653+bradscot@users.noreply.github.com>
2023-03-19 21:09:35 -0500

    well then

--- app.py
+++ app.py
@@ -7,12 +7,20 @@ from sqlalchemy import create_engine, text
 import io
 import base64
 
+
+
+
 app = Flask(__name__)
+
+
+
 
 @app.route('/')
 def index():
     print('Request for index page received')
     return render_template('index.html')
+
+
 
 
 @app.route('/favicon.ico')
@@ -21,7 +29,6 @@ def favicon():
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
-@app.route('/hello', methods=['POST'])
 
 
 @app.route('/hello', methods=['POST'])
@@ -30,6 +37,9 @@ def hello():
     if zip:
         print('Request for hello page received with zip=%s' % zip)
 
+
+
+
         # Insert the zip code into the database
         server = 'quotechies.database.windows.net'
         database = 'quotechies-db'
@@ -37,16 +47,28 @@ def hello():
         password = 'hackathon10!'
         driver = '{ODBC Driver 17 for SQL Server}'
 
+
+
+
         # Create the connection string
         connection_string = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}"
+
+
+
 
         # Set up the connection
         engine = create_engine(connection_string)
         conn = engine.connect()
 
+
+
+
         # Insert zip code
         query = text("INSERT INTO session2 (zip_code) VALUES (:zip)")
         conn.execute(query, zip=zip)
+
+
+
 
         # Query the data for the graph
         query = text("""
@@ -63,6 +85,9 @@ def hello():
         """)
         data = conn.execute(query)
 
+
+
+
         # Create a dictionary to store the data
         graph_data = {}
         for row in data:
@@ -76,19 +101,28 @@ def hello():
                 }
             graph_data[reason][season.lower()] = avg_price
 
+
+
+
         # Close the database connection
         conn.close()
+
+
+
 
         # Create a line chart
         fig, ax = plt.subplots()
         for reason, values in graph_data.items():
-            x = ['spring', 'summer', 'fall', 'winter']
-            y = [values['spring'], values['summer'], values['fall'], values['winter']]
+            x = ['Spring', 'Summer', 'Fall', 'Winter']
+            y = [values['Spring'], values['Summer'], values['Fall'], values['Winter']]
             ax.plot(x, y, label=reason)
         ax.set_xlabel('Season')
         ax.set_ylabel('Average Price')
         ax.set_title('Energy Surge Costs Due to Outages by Season')
         ax.legend()
+
+
+
 
         # Encode the image as a base64 string
         buffer = io.BytesIO()
@@ -96,12 +130,26 @@ def hello():
         buffer.seek(0)
         image_string = base64.b64encode(buffer.getvalue()).decode()
 
+
+
+
         # Render the template with the image embedded
         return render_template('hello.html', zip=zip, image_string=image_string)
+
+
+
 
     else:
         print('Request for hello page received with no name or blank name -- redirecting')
         return redirect(url_for('index'))
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 
 
 
