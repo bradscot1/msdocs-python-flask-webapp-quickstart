592b53a7388660835fe7703240cdbb1cd0517513
FZNH97 <70118653+bradscot@users.noreply.github.com>
2023-03-19 20:31:45 -0500

    deploy

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from sqlalchemy import create_engine, text
+import io
+import base64
 
 app = Flask(__name__)
 
@@ -20,11 +22,13 @@ def favicon():
 
 
 @app.route('/hello', methods=['POST'])
+
+
+@app.route('/hello', methods=['POST'])
 def hello():
     zip = request.form.get('zip')
     if zip:
         print('Request for hello page received with zip=%s' % zip)
-
 
         # Insert the zip code into the database
         server = 'quotechies.database.windows.net'
@@ -33,61 +37,71 @@ def hello():
         password = 'hackathon10!'
         driver = '{ODBC Driver 17 for SQL Server}'
 
-
         # Create the connection string
         connection_string = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}"
-
 
         # Set up the connection
         engine = create_engine(connection_string)
         conn = engine.connect()
 
-
         # Insert zip code
         query = text("INSERT INTO session2 (zip_code) VALUES (:zip)")
         conn.execute(query, zip=zip)
 
-
         # Query the data for the graph
         query = text("""
-            SELECT Season, Reason, AVG(Avg_Price) 
-            FROM weather_incidents 
-            GROUP BY Season, Reason 
-            ORDER BY 
-                CASE Season 
-                    WHEN 'Spring' THEN 1 
-                    WHEN 'Summer' THEN 2 
-                    WHEN 'Fall' THEN 3 
-                    WHEN 'Winter' THEN 4 
+            SELECT Season, Reason, AVG(Avg_Price)
+            FROM weather_incidents
+            GROUP BY Season, Reason
+            ORDER BY
+                CASE Season
+                    WHEN 'Spring' THEN 1
+                    WHEN 'Summer' THEN 2
+                    WHEN 'Fall' THEN 3
+                    WHEN 'Winter' THEN 4
                 END
         """)
         data = conn.execute(query)
 
-
         # Create a dictionary to store the data
         graph_data = {}
         for row in data:
-            Season, Reason, Avg_Price = row
-            if Reason not in graph_data:
-                graph_data[Reason] = {
-                    'Spring': 0,
-                    'Summer': 0,
-                    'Fall': 0,
-                    'Winter': 0
+            season, reason, avg_price = row
+            if reason not in graph_data:
+                graph_data[reason] = {
+                    'spring': 0,
+                    'summer': 0,
+                    'fall': 0,
+                    'winter': 0
                 }
-            graph_data[Reason][Season.lower()] = Avg_Price
-
+            graph_data[reason][season.lower()] = avg_price
 
         # Close the database connection
         conn.close()
 
+        # Create a line chart
+        fig, ax = plt.subplots()
+        for reason, values in graph_data.items():
+            x = ['spring', 'summer', 'fall', 'winter']
+            y = [values['spring'], values['summer'], values['fall'], values['winter']]
+            ax.plot(x, y, label=reason)
+        ax.set_xlabel('Season')
+        ax.set_ylabel('Average Price')
+        ax.set_title('Energy Surge Costs Due to Outages by Season')
+        ax.legend()
 
-        return render_template('hello.html', zip=zip, graph_data=graph_data)
+        # Encode the image as a base64 string
+        buffer = io.BytesIO()
+        fig.savefig(buffer, format='png')
+        buffer.seek(0)
+        image_string = base64.b64encode(buffer.getvalue()).decode()
+
+        # Render the template with the image embedded
+        return render_template('hello.html', zip=zip, image_string=image_string)
+
     else:
         print('Request for hello page received with no name or blank name -- redirecting')
         return redirect(url_for('index'))
-
-
 
 
 
