1b91849b6241c780486b17e95942dfb6168a03db
FZNH97 <70118653+bradscot@users.noreply.github.com>
2023-03-20 00:11:34 -0500

    hm

--- app.py
+++ app.py
@@ -13,103 +13,107 @@ import json
 app = Flask(__name__)
 app.config['REFERRER_POLICY'] = 'no-referrer-when-downgrade'
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/hello', methods=['POST'])
 def hello():
-    
-        zip_code = request.form.get('zip')
-        if not zip_code:
-            return redirect(url_for('index'))
-        else:
-            server = 'quotechies.database.windows.net'
-            database = 'quotechies-db'
-            username = 'bscott129@quotechies'
-            password = 'hackathon10!'
-            driver = '{ODBC Driver 17 for SQL Server}'
+    zip_code = request.form.get('zip')
+    if not zip_code:
+        return redirect(url_for('index'))
 
 
-            connection_string = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}"
-            engine = create_engine(connection_string)
-            conn = engine.connect()
+    server = 'quotechies.database.windows.net'
+    database = 'quotechies-db'
+    username = 'bscott129@quotechies'
+    password = 'hackathon10!'
+    driver = '{ODBC Driver 17 for SQL Server}'
 
 
-            query = text("INSERT INTO session2 (zip_code) VALUES (:zip)")
-            conn.execute(query, zip=zip_code)
+    connection_string = f"mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}"
+    engine = create_engine(connection_string)
+    conn = engine.connect()
 
 
-            query = text("""
-                SELECT Season, Reason, AVG(Avg_Price) AS Avg_Price
-                FROM weather_incidents
-                GROUP BY Season, Reason
-                ORDER BY
-                    CASE Season
-                        WHEN 'Spring' THEN 1
-                        WHEN 'Summer' THEN 2
-                        WHEN 'Fall' THEN 3
-                        WHEN 'Winter' THEN 4
-                    END
-            """)
-            data = conn.execute(query)
+    query = text("INSERT INTO session2 (zip_code) VALUES (:zip)")
+    conn.execute(query, zip=zip_code)
 
 
-            graph_data = {}
-            for row in data:
-                Season, Reason, Avg_Price = row
-                if Reason not in graph_data:
-                    graph_data[Reason] = {
-                        'Spring': 0,
-                        'Summer': 0,
-                        'Fall': 0,
-                        'Winter': 0
-                    }
-                graph_data[Reason][Season] = Avg_Price
+    query = text("""
+        SELECT Season, Reason, AVG(Avg_Price) AS Avg_Price
+        FROM weather_incidents
+        GROUP BY Season, Reason
+        ORDER BY
+            CASE Season
+                WHEN 'Spring' THEN 1
+                WHEN 'Summer' THEN 2
+                WHEN 'Fall' THEN 3
+                WHEN 'Winter' THEN 4
+            END
+    """)
+    data = conn.execute(query)
 
 
-            conn.close()
-
-
-            x_labels = ['Spring', 'Summer', 'Fall', 'Winter']
-            datasets = []
-            for Reason, values in graph_data.items():
-                data = [values['Spring'], values['Summer'], values['Fall'], values['Winter']]
-                datasets.append({
-                    'label': Reason,
-                    'data': data,
-                    'borderColor': 'red',
-                    'fill': False
-                })
-
-
-            chart_data = {
-                'type': 'line',
-                'data': {
-                    'labels': x_labels,
-                    'datasets': datasets
-                },
-                'options': {
-                    'scales': {
-                        'yAxes': [{
-                            'ticks': {
-                                'beginAtZero': True
-                            }
-                        }]
-                    }
-                }
+    graph_data = {}
+    for row in data:
+        season, reason, avg_price = row
+        if reason not in graph_data:
+            graph_data[reason] = {
+                'Spring': 0,
+                'Summer': 0,
+                'Fall': 0,
+                'Winter': 0
             }
+        graph_data[reason][season] = avg_price
 
 
-            chart_json = json.dumps(chart_data)
+    conn.close()
 
 
-            return render_template('hello.html', zip=zip_code, chart_data=chart_json)
+    x_labels = ['Spring', 'Summer', 'Fall', 'Winter']
+    datasets = []
+    for reason, values in graph_data.items():
+        data = [values['Spring'], values['Summer'], values['Fall'], values['Winter']]
+        datasets.append({
+            'label': reason,
+            'data': data,
+            'borderColor': 'red',
+            'fill': False
+        })
 
 
-    
+    chart_data = {
+        'type': 'line',
+        'data': {
+            'labels': x_labels,
+            'datasets': datasets
+        },
+        'options': {
+            'scales': {
+                'yAxes': [{
+                    'ticks': {
+                        'beginAtZero': True
+                    }
+                }]
+            }
+        }
+    }
+
+
+    chart_json = json.dumps(chart_data)
+
+
+    return render_template('hello.html', zip=zip_code, chart_data=chart_json)
 
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+
+
+
